bbb36982957b3dd1bcd8b890f3da53976d4535a8
greta <greta.belegu@gmail.com>
2023-05-25 14:07:10 -0400

    new changes & more jquery

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+          window.location.hash = hash;
+        });
+      } 
+    });
+
+    $("button").click(function() {
+        var $this = $(this);
+        if($this.hasClass("more")){
+        $( ".content" ).slideToggle('500','linear' );
+                $this.toggleClass("open");
+                if ($this.hasClass("open")) {
+                    $this.html("Hide");
+                } else {
+                    $this.html("Show previous");
+                }
+            }
+    });
+
+    $("button-1").click(function() {
+        var $this = $(this);
+        if($this.hasClass("more1")){
+        $( ".content-1" ).slideToggle('500','linear' );
+                $this.toggleClass("open-1");
+                if ($this.hasClass("open-1")) {
+                    $this.html("Show more");
+                } else {
+                    $this.html("Show less");
+                }
+            }
+    });
+});
